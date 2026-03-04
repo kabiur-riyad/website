56d4ced1ea0594797ba08b9df106f943b5210fa2
Kabiur Rahman Riyad <actualriyad@protonmail.com>
2026-03-05 01:06:05 +0600

    Show legacy photos on home grid

--- lib/sanity.queries.ts
+++ lib/sanity.queries.ts
@@ -1,5 +1,5 @@
 export const photoGridQuery = `
-  *[_type == "photo" && defined(image) && homeVisible == true] | order(publishedAt desc) {
+  *[_type == "photo" && defined(image) && homeVisible != false] | order(publishedAt desc) {
     _id,
     title,
     caption,
