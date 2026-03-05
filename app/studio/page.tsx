b96d88d0ed3d86c477cfc370222056bc88fa2cc3
Kabiur Rahman Riyad <actualriyad@protonmail.com>
2026-03-05 12:58:34 +0600

    Fix studio redirect hostname

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -1,9 +1,8 @@
 import { redirect } from "next/navigation";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "1descy2t";
 const studioUrl =
   process.env.NEXT_PUBLIC_SANITY_STUDIO_URL ||
-  `https://${projectId}.sanity.studio`;
+  "https://riyad-site.sanity.studio";
 
 export default function StudioRedirect() {
   redirect(studioUrl);
